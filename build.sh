eee485ec4e99f2635811cea3da10b18c4b758795
bilal12367 <sk.bilal.md@gmail.com>
2023-12-31 22:07:00 +0530

    commit8

--- build.sh
+++ build.sh
@@ -2,19 +2,21 @@ npm config set registry "http://registry.npmjs.org/"
 export CI=false
 npm install
 # Check if the directory exists
-if [ -d "build" ]; then
-    echo "Removing directory: build"
-    rm -rf "build"
-else
-    echo "Directory does not exist: $directoryPath"
-fi
+# if [ -d "build" ]; then
+#     echo "Removing directory: build"
+#     rm -rf "build"
+# else
+#     echo "Directory does not exist: $directoryPath"
+# fi
 
-if [ -d "./server/build" ]; then
-    echo "Removing directory: build"
-    rm -rf "./server/build"
-else
-    echo "Directory does not exist: $directoryPath"
-fi
+# if [ -d "./server/build" ]; then
+#     echo "Removing directory: build"
+#     rm -rf "./server/build"
+# else
+#     echo "Directory does not exist: $directoryPath"
+# fi
+rm -rf "build"
+rm -rf "./server/build"
 
 npm run build
 mv build ./server
